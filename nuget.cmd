17b7b8f9b36a05d7973a8326d2c9abcd89c5edcd
John Hoerr <jhoerr@gmail.com>
2012-08-31 12:53:21 -0400

    Adding nuget packaging pieces.

---
+++ nuget.cmd
@@ -0,0 +1,12 @@
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe SkyNet.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
+
+del nuget\* /F /Q
+
+mkdir nuget\lib\net40\
+
+copy SkyNet\bin\Release\SkyNet.dll nuget\lib\net40
+copy SkyNet\bin\Release\SkyNet.xml nuget\lib\net40
+
+nuget.exe update -self
+nuget.exe pack SkyNet.nuspec -BasePath nuget -Output nuget
+
